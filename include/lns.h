784ac88ff09ca39db5955ab778a3afb8fe62eb64
heroi17 <sharouskaya3@yandex.ru>
2024-03-24 20:50:24 +0300

    base structure already done, should to solwe many todo.

--- include/lns.h
+++ include/lns.h
@@ -3,8 +3,11 @@
 #define _INCLUDE_LNS_H_
 #include <iostream>
 #include "bitset.h"
-
+#if SIMPLE_REALIZATION
 #define STANDARD_BITSET_TYPE BITSET
+#else
+#define STANDARD_BITSET_TYPE BITSET<>
+#endif // SIMPLE_REALIZATION
 
 template <class NUMTYPE = STANDARD_BITSET_TYPE>
 class lns
@@ -15,32 +18,35 @@ protected:
 
 	lns(bool isPositive, NUMTYPE logNumber) : _isPositive(isPositive), _logNumber(logNumber) {}
 public:
-
+	// -----------------(DEBUG FUNCTION)------------------------
+	void print()
+	{ 
+		std::cout << "(" << (this->_isPositive ? '+' : '-') << ")(" << this->_logNumber << ")" << std::endl;
+	}
+	// ------------------(init function)------------------------
 	lns(const lns& other) = default;
+
 	lns() = default;
 
 	template<typename oldType>
 	lns(const oldType& number)
 	{
-		//Before i think we can use only one times for object it's init operator.
-		if (number >= 0)	this->lns::lns(true, std::log2(number));
-		else				this->lns::lns(false, std::log2(number * (-1)));
+		if (number >= 0)	this->lns::lns(true, (double)std::log2((double)number));
+		else				this->lns::lns(false, (double)std::log2((double)number * (-1)));
 	}
-	void print()
-	{
-		std::cout << "(" << (this->_isPositive ? '+' : '-') << ")(" << this->_logNumber << ")" << std::endl;
-	}
+
+	// ---------------------(math operators)--------------------
 	lns operator *(const lns& other) const
 	{
 		return lns(this->_isPositive == other._isPositive, this->_logNumber + other._logNumber);
 	}
 
 	lns operator /(const lns& other) const
-	{
+	{ // todo: maybe add check if other = 0+ or 0-
 		return lns(this->_isPositive == other._isPositive, this->_logNumber - other._logNumber);
 	}
 
-	lns& operator*=(const lns& other)
+	lns& operator*=(const lns& other) // as easyy as it is.
 	{
 		this->_isPositive = (this->_isPositive == other._isPositive);
 		this->_logNumber += other._logNumber;
@@ -56,25 +62,25 @@ public:
 
 	lns operator+(const lns& other)
 	{
-		return lns((NUMTYPE)(*this) + (NUMTYPE)(other)); // TODO: chenge it for not use lns and int.
+		return lns((NUMTYPE)(*this) + (NUMTYPE)(other)); // TODO: chenge it for calculate it wis cpecial formula x + s(y - x).
 	}
 
 	lns operator-(const lns& other)
 	{
-		return lns((NUMTYPE)(*this) - (NUMTYPE)(other)); // TODO: chenge it for not use lns and int.
+		return lns((NUMTYPE)(*this) - (NUMTYPE)(other)); // TODO: chenge it for calculate it wis cpecial formula x + d(y - x).
 	}
 
 	lns& operator+=(const lns& other)
 	{
-		this->lns::lns(*this + other); // TODO: chenge it for not use lns and int.
+		this->lns::lns(*this + other); // TODO: make it faster without makeing new lns us formula.
 		return *this;
 	}
 	lns& operator-=(const lns& other)
 	{
-		this->lns::lns(*this - other); // TODO: chenge it for not use lns and int.
+		this->lns::lns(*this - other); // TODO: make it faster without makeing new lns us formula.
 		return *this;
 	}
-	lns operator-() const
+	lns operator-() const //It's good as for me.
 	{
 		return lns(!_isPositive, this->_logNumber);
 	}
@@ -82,14 +88,20 @@ public:
 	{
 		return *this;
 	}
+	// ------------------(compare operators)-----------------------------------
+	
+	//TODO: add operator>
+	//TODO: add operator>=
+	//TODO: add operator<=
+	//TODO: add operator!=
 
-	bool operator==(const lns& other) // Here exist problm: if this = -0 and other = +0 then it work wrong. TODO: solwe it.
+	bool operator==(const lns& other) // TODO: Here exist problem: if this = -0 and other = +0 then it work wrong. TODO: solwe it. or think how to solwe it
 	{
 		return (this->_isPositive == other._isPositive) && (this->_logNumber == other._logNumber);
 	}
 
 	bool operator<(const lns& other)
-	{ // Here exist problm: if this = -0 and other = +0 then it work wrong. TODO: solwe it.
+	{ //TODO: Here exist problm: if this = -0 and other = +0 then it work wrong. TODO: solwe it.
 		if (this->_isPositive)
 		{
 			if (other._isPositive)
@@ -103,22 +115,23 @@ public:
 			return this->_logNumber > other._logNumber;
 		}
 	}
-	// Convert to any type!!! it's ausem that we can do like down here.
-
+	// ---------------(to type operators)---------------------------
 	template <typename OUTTYPE>
 	operator OUTTYPE() const
 	{
-		//std::cout << "( " << (_logNumber - 8) << ") ";
+		//TODO: we should to check size of _logNumber, if it too big we got a problem
 		if (_isPositive)
-			return std::pow(2, _logNumber);
-		return -std::pow(2, _logNumber);
+			return std::pow(2, (double)_logNumber);
+		return -std::pow(2, (double)_logNumber);
 	}
 
 
 
 };
+
 template <typename LNSTYPE>
 std::ostream& operator<< (std::ostream& os, const lns<LNSTYPE>& m) {
+	// TODO:here problem if number is too big we lost data when use (double)m, so we need to better wariant writing the answer.
 	os << (double)m;
 	return os;
 }
--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
 #include "include/lns.h"
+#include "include/bitset.h"
 #include <complex>
-int main()
+template<typename NUM_TYPE>
+void test(NUM_TYPE a, NUM_TYPE b)
 {
-	using complexLns = std::complex<lns<>>;
-
-	auto a = complexLns(1000000, 0);
-	auto b = complexLns(0, 10000000);
 	std::cout << a << " + " << b << " = " << a + b << std::endl;
 	std::cout << a << " - " << b << " = " << a - b << std::endl;
 	std::cout << a << " / " << b << " = " << a / b << std::endl;
 	std::cout << a << " * " << b << " = " << a * b << std::endl;
+}
+int main()
+{
+	using complexLns = std::complex<lns<>>;
+	BITSET aa = 5.555;
+	BITSET bb = 4.444;
+	test(aa, bb);
+
+	auto a = complexLns(1E5, 1E2);
+	auto b = complexLns(1E3, 1E4);
+	test(a, b);
 	return 0;
 }
