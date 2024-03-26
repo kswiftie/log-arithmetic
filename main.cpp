4d204cefd929533b291391ea5306df7ae56efa19
heroi17 <sharouskaya3@yandex.ru>
2024-03-26 19:29:39 +0300

    Markov add bitset.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+﻿#include "include/lns.h"
+#include "include/bitset.h"
+#include <complex>
+#include <iostream>
+template<typename NUM_TYPE>
+void test(NUM_TYPE a, NUM_TYPE b)
+{
+	std::cout << a << " + " << b << " = " << a + b << std::endl;
+	std::cout << a << " - " << b << " = " << a - b << std::endl;
+	std::cout << a << " / " << b << " = " << a / b << std::endl;
+	std::cout << a << " * " << b << " = " << a * b << std::endl;
+}
+int main()
+{
+	//using complexLns = std::complex<lns<>>;
+	lns aa = 5.555;
+	lns bb = 4.444;
+	test(aa, bb);
+
+	//auto a = complexLns(1E5, 1E2);
+	//auto b = complexLns(1E3, 1E4);
+	//test(a, b);
+	return 0;
+}
